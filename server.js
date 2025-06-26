2717134d7188db15c9a6f21103d44006ed808941
ReenactmentPark <78677280+ReenactmentPark@users.noreply.github.com>
2025-06-26 16:13:09 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
